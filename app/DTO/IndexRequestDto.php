fa8e9da3de730f5172ebd786a57a59a076719669
Максим Кашкин <m.kashkin@devspark.ru>
2023-02-05 20:04:24 +0300

    Remove user_id from index request dto

--- app/DTO/IndexRequestDto.php
+++ app/DTO/IndexRequestDto.php
@@ -11,15 +11,12 @@ class IndexRequestDto extends DataTransferObject
     public ?int $perPage;
     public ?array $filters;
 
-    public ?int $userId;
-
     public static function fromRequest(Request $request)
     {
         return new self([
             'page' => $request->get('page'),
             'perPage' => $request->get('per-page'),
             'filters' => $request->get('filters'),
-            'userId' => $request->user()?->getAuthIdentifier(),
         ]);
     }
 }
